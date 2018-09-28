33565077fbd70b4757c8a45f688490f888b4e195
oussrh <ousrh7@gmail.com>
2018-09-28 12:04:49 +0200

    excerices objet

---
+++ ex6/script.js
@@ -0,0 +1,9 @@
+var num1  = prompt("Enter a  first float number please (use the .)", "");
+
+var num2  = prompt("Enter a second float number please (use the .)", "");
+
+var resultat =  num1%num2;
+
+
+
+alert("Hello my friend this is the result " +  resultat );
