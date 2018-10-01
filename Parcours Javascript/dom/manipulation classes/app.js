ec5d4e0989b9df7d6834fb2c3e8a328fd0ca62c9
oussrh <ousrh7@gmail.com>
2018-10-01 14:07:28 +0200

    fini personnage bonus

---
+++ Parcours Javascript/dom/manipulation classes/app.js
@@ -0,0 +1,9 @@
+
+//let longeur = document.querySelectorAll(".bg-lime").length;
+
+laClass = document.querySelectorAll('.bg-lime');
+
+for(i=0; i< laClass.length; i++){
+
+    laClass[i].classList.add('ffff');
+}
